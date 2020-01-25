f3ac71b54755a73e86ab2ea6f2b274fe3a9c9458
Aetra <vava-hajek@hotmail.fr>
2020-01-25 20:04:46 +0100

    FixBaseProject

--- JeanSaitRien/src/app/app-routing.module.ts
+++ JeanSaitRien/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
+import { TestComponent } from "./test/test.component";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
@@ -8,7 +9,8 @@ import { ItemDetailComponent } from "./item/item-detail.component";
 const routes: Routes = [
     { path: "", redirectTo: "/items", pathMatch: "full" },
     { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "item/:id", component: ItemDetailComponent},
+    { path: "test", component: TestComponent }
 ];
 
 @NgModule({
